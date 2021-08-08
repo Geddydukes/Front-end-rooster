c0f663ff62296ed372cc3db283d7a8339a9006f4
gdukes707 <Gdukes707@gmail.com>
2021-08-07 20:28:20 -0700

    attempting heroku deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
