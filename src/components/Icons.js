b252b76d51bcfe729deb06506784f356305bd663
gdukes707 <Gdukes707@gmail.com>
2021-08-11 16:52:51 -0700

    changes to home icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -7,26 +7,30 @@ import homeRetention from "../front-end-dev-test-assets/home-retention.svg";
 const Icons = () => {
   return (
     <div className="iconsContainer">
-      <img
-        src={homeExpertise}
-        alt="homeExpertise Logo"
-        className="homeIcons homeIcons1"
-      ></img>{" "}
-      <img
-        src={homeHygiene}
-        alt="homeHygiene Logo"
-        className="homeIcons homeIcons2"
-      ></img>{" "}
-      <img
-        src={homeLab}
-        alt="homeLab Logo"
-        className="homeIcons homeIcons3"
-      ></img>{" "}
-      <img
-        src={homeRetention}
-        alt="homeRetention Logo"
-        className="homeIcons homeIcons4"
-      ></img>
+      <div className="iconBorder homeIcons1">
+        <img
+          src={homeExpertise}
+          alt="homeExpertise Logo"
+          className="homeIcons"
+        ></img>{" "}
+      </div>
+      <div className="iconBorder homeIcons2">
+        <img
+          src={homeHygiene}
+          alt="homeHygiene Logo"
+          className="homeIcons"
+        ></img>{" "}
+      </div>
+      <div className="iconBorder homeIcons3">
+        <img src={homeLab} alt="homeLab Logo" className="homeIcons"></img>{" "}
+      </div>
+      <div className="iconBorder homeIcons4">
+        <img
+          src={homeRetention}
+          alt="homeRetention Logo"
+          className="homeIcons"
+        ></img>
+      </div>
     </div>
   );
 };
